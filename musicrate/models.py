67a986b819abfab22564296a55dba58cb3026e68
blakemaune <bfm7@miami.edu>
2020-03-01 21:07:55 -0600

    Added basic Profile model

--- musicrate/models.py
+++ musicrate/models.py
@@ -3,6 +3,18 @@ from django.db import models
 from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 class Album(models.Model):
     spotify_id = models.CharField(primary_key=True, max_length=200)
@@ -31,3 +43,14 @@ class Review(models.Model):
 
     def __str__(self):
         return self.author.__str__() + " rated " + self.album.title + " by " + self.album.artist + " " + self.rating.__str__()
+
+# -- START RECEIVERS TO LINK PROFILE MODEL TO USER MODEL --#
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+# -- END  RECEIVERS TO LINK PROFILE MODEL TO USER MODEL --#
